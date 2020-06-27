9d5f398dd9626f78535c9b5113e927c1087f4eee
abdmalique hadge <yain80000@hotmail.com>
2020-06-26 22:41:37 -0500

    compined mutiple files into one basics file for easy maintanance

--- intro/basics.py
+++ intro/basics.py
@@ -1,3 +1,22 @@
+print(5 + 6)
+print(56 // 4)
+print(45**3)
+print(4.5 * 5)
+print(1/6)
+print(-54 + 5)
+
+# more examples
+print("This one we have used before")
+print(len("This one prints the number of characters in the string"))
+print(abs(-43.6234422))
+print(type(56.4))
+print(type(45))
+print(type("hello python"))
+print(round(4563.4567))
+print(max(8,3,4,2,22,432))
+print(min(45,22,3,2,6,7))
+
+# and more basics
 # variable is a placeholder for a value
 salary = 24000
 house_rent = 1000
@@ -16,8 +35,3 @@ print("The course will cost ", total_fee)
 # more variable example
 x = "awesome"
 print("python is " + x)
-
-
-
-
-
--- intro/mathoperations.py
+++
@@ -1,6 +0,0 @@
-print(5 + 6)
-print(56 // 4)
-print(45**3)
-print(4.5 * 5)
-print(1/6)
-print(-54 + 5)
