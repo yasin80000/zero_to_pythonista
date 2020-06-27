aca44b81ed4b5f83313a5e6b910b2c53a136508d
abdmalique hadge <yain80000@hotmail.com>
2020-06-26 21:52:45 -0500

    more variable example exercises

---
+++ intro/variables.py
@@ -0,0 +1,23 @@
+# variable is a placeholder for a value
+salary = 24000
+house_rent = 1000
+food_expenses = 4000
+total_expenses = house_rent + food_expenses
+total_savings = salary - total_expenses
+print("this is the total expenses ", str(total_expenses))
+print("this is how much saved ", str(total_savings))
+
+# more examples
+course_fee = 500
+num_of_months = 6
+total_fee = course_fee * num_of_months
+print("The course will cost ", total_fee)
+
+# more variable example
+x = "awesome"
+print("python is " + x)
+
+
+
+
+
